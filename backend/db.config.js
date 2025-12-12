82369becd308a75c29c7ee475644de990847141c
Rmendevsec <ezrawork12@gmail.com>
2025-12-12 16:57:00 +0300

    Add database configuration with Sequelize and dotenv for environment variables

---
+++ backend/db.config.js
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const sequelize = new Sequelize{
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        logging: false
+
+    }
+}
+
+(async ()=>{
+    try{
+        await sequelize.authenticate()
+        console.log("Database connected")
+
+    }
+    catch(err){
+        console.error("Unable to connect to the database:", err)
+    }
+})()
+
+export default sequelize
